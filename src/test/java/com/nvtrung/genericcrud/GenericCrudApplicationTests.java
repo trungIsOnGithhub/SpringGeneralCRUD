c6a040ba4e27deae941dae31bc629a5a95457414
trungIsOnGithhub <trung150502@gmail.com>
2023-09-19 14:11:50 +0700

    initial setup commit

---
+++ src/test/java/com/nvtrung/genericcrud/GenericCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvtrung.genericcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
