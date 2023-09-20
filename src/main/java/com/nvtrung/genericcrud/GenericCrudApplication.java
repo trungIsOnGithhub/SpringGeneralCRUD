62b5a1a4687fa8a37b5272442341bb8d833bb955
unknown <Administrator@MAY34.MAY29>
2023-09-20 21:43:50 +0700

    modified application

--- src/main/java/com/nvtrung/genericcrud/GenericCrudApplication.java
+++ src/main/java/com/nvtrung/genericcrud/GenericCrudApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GenericCrudApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GenericCrudApplication.class, args);
 	}
