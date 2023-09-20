3cc5aa218c0d7e0d8aa7a9af6f1adddcaeb102b3
unknown <Administrator@MAY34.MAY29>
2023-09-20 21:42:48 +0700

    modified controller layer

--- src/main/java/com/nvtrung/genericcrud/controller/GenericItemController.java
+++ src/main/java/com/nvtrung/genericcrud/controller/GenericItemController.java
@@ -3,8 +3,13 @@ package com.nvtrung.genericcrud.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.nvtrung.genericcrud.model.GenericItem;
 import com.nvtrung.genericcrud.service.GenericItemService;
 
 @Controller
@@ -14,8 +19,35 @@ public class GenericItemController {
 
 	// display list of item
 	@GetMapping("/")
-	public String viewHomePage(Model model) {
+	public String viewItemHomeList(Model model) {
 		model.addAttribute("listItems", itemService.getAllItem());
 		return "index";
 	}
+	
+	@GetMapping("/new")
+	public String addNewItemForm(Model model) {
+		GenericItem newItem = new GenericItem();
+		model.addAttribute("newItem", newItem);
+		return "new";
+	}
+	
+	 @PostMapping("/new")
+	 public String saveEmployee(@ModelAttribute("newItem") GenericItem newItem) {
+	     // save employee to database
+		 this.itemService.addOneItem(newItem);
+	     return "redirect:/";
+	 }
+	 
+	 @GetMapping("/update/{id}")
+	 public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+		GenericItem item = itemService.getItemById(id);
+		model.addAttribute("foundItem", item);
+		return "update";
+	 }
+	 
+	 @GetMapping("/delete/{id}")
+	 public String deleteEmployee(@PathVariable(value = "id") long id) {
+		 this.itemService.deleteItemById(id);
+		 return "redirect:/";
+	 }
 }
